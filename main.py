820dfde17775177505a2422016cfc0ebf2f67995
K <koftyan@gmail.com>
2020-07-08 18:58:50 +0300

    add posibility to give a git username

--- main.py
+++ main.py
@@ -9,12 +9,14 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument("apiKey", help = 'GitLab key with "read API" scope. (Go to GitLab -> User settings -> Access tokens. Give name and expiration date, check "read_api" option and press "Create personal access token"')
 parser.add_argument("gitlabUrl", help = 'URL to GitLab, e.g. https://my.git.lab.url')
+parser.add_argument("--gituser", help = 'GitLab username')
 
 args = parser.parse_args()
 
 gitlabUrl = args.gitlabUrl # URL to GitLab 
-projectsPerPage = 10
 key = args.apiKey # GitLab key with "read API" scope
+gitUser = args.gituser if args.gituser else ''
+projectsPerPage = 10
 baseUrl = '{}/api/v4/projects?simple=true&per_page={}&private_token={}'
 url = baseUrl.format(gitlabUrl, projectsPerPage, key)
 
@@ -29,6 +31,8 @@ while True:
     project_name = project['name']
     project_path = project['namespace']['full_path']
     project_url = project['ssh_url_to_repo']
+    if gitUser:
+      project_url = project_url.replace('git@', f'{gitUser}@')
     os.chdir(baseDir)
 
     print('\nProcessing %s...' % project_name)
